ad8caa195afb126a4cc21f70a1fe87235d7c3f71
AnhTuan2k1 <anhtuanat2k1@gmail.com>
2022-10-26 21:34:25 +0700

    Data models, Service, Controller, Database: Sql Server+ EntitiFramework

--- CinemaBlazor/Client/Program.cs
+++ CinemaBlazor/Client/Program.cs
@@ -1,3 +1,4 @@
+using CinemaBlazor.Client.Services;
 using CinemaBlazor.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -7,5 +8,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<IFilmService, FilmService>();
+builder.Services.AddScoped<IFilmGenreService, FilmGenreService>();
 
 await builder.Build().RunAsync();
